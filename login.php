6a2e37fe8e408f0c375b6781cea8fbcad8d1bf14
PrinceMargaret <princeagrahari2000@gmail.com>
2021-05-27 22:20:29 -0500

    login css

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
     <title>Untitled</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/assets/css/styles.min.css?h=fbb6300924f3f2eafb13a0b441cd0e3a">
+    <link rel="stylesheet" href="login.css">
 </head>
 
 <body>
